6a5923fb18f17a3f584914223f3be838b15f15ab
DanThing <dan@boycething.com>
2018-04-23 20:01:32 +1000

    Create Skills.vb

---
+++ EP/Skills.vb
@@ -0,0 +1,17 @@
+Public Class Skills
+
+    Public Category As String
+
+    Public Linked As String
+
+    Public desc As IDictionary
+
+    Public Sub New()
+    
+        desc.Add("useage", "")
+        desc.Add("spec", "")
+        desc.Add("desc", "")
+    
+    End Sub
+    
+End Class
